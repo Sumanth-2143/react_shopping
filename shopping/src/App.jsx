c4cfe33c37efe91b8eccaf376b9d42dca39b091a
Sumanth Achakala <sumanthbabu@gmail.com>
2024-05-03 22:55:42 +0530

    shopping react

---
+++ shopping/src/App.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+import './App.css'
+import MainPage from './shoppingfolder/pages/MainPage'
+
+const App = () => {
+  return (
+    <div>
+      
+      <MainPage />
+      
+
+    </div>
+  )
+}
+
+export default App
---
+++ shopping/src/shoppingfolder/data.js
@@ -0,0 +1,32 @@
+export const Gents = {
+    title: "Gents Fashion",
+    image1: "assest/Men/1.jpg",
+    image2: "assest/Men/2.jpg",
+    image3: "assest/Men/3.jpg",
+    image4: "assest/Men/4.jpg",
+    image5: "assest/Men/5.jpg",
+    image6: "assest/Men/6.jpg",
+    price1: "559 INR",
+    price2: "459 INR",
+    price3: "659 INR",
+    price4: "759 INR",
+    price5: "859 INR",
+    price6: "959 INR",
+}
+
+export const Ladies = {
+    title: "Ladies Fashion",
+    image1: "assest/Woman/1.jpg",
+    image2: "assest/Woman/2.jpg",
+    image3: "assest/Woman/3.jpg",
+    image4: "assest/Woman/4.jpg",
+    image5: "assest/Woman/5.jpg",
+    image6: "assest/Woman/6.jpg",
+    price1: "1559 INR",
+    price2: "1459 INR",
+    price3: "1659 INR",
+    price4: "1759 INR",
+    price5: "1859 INR",
+    price6: "1959 INR",
+
+}
